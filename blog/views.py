1771fc9f3d400557e1eb54c8bfb7fcf67ce306c2
zhangdy <zhangdy@seniorkeeper.com>
2017-10-18 09:54:43 +0800

    d

--- blog/views.py
+++ blog/views.py
@@ -12,3 +12,8 @@ def index(request):
     article = models.Article.objects.get(pk=1)
     #return render(request, "blog/index.html", {"hello": "hello blog"})
     return render(request, "blog/index.html", {"article": article})
+
+
+def edit_page(request, article_id):
+    article = models.Article.objects.get(pk=1)
+    return render(request, "blog/edit_page.html", {"article": article})
